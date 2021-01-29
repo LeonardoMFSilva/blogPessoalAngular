b3fcab522f66e12e76be072203883ca8a1de299a
Leonardo <you@example.com>
2021-01-29 11:46:46 -0300

    Adding Put method to 'usuario'

---
+++ src/app/edit/user-edit/user-edit.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Usuario } from 'src/app/model/Usuario';
+import { AuthService } from 'src/app/service/auth.service';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-user-edit',
+  templateUrl: './user-edit.component.html',
+  styleUrls: ['./user-edit.component.css']
+})
+export class UserEditComponent implements OnInit {
+
+
+  usuario: Usuario = new Usuario()
+  idUser: number
+  confirmarSenha: string
+  tipoUser: string
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() { 
+    window.scroll(0,0)
+
+    if(environment.token == ""){
+      this.router.navigate(["/entrar"])
+    }
+
+    this.idUser = this.route.snapshot.params["id"]
+    this.findByIdUser(this.idUser)
+   }
+
+   confirmaSenha(event: any){
+    this.confirmarSenha = event.target.value
+   }
+
+   tipoUsuario(event: any){
+    this.tipoUser = event.target.value
+   }
+
+   atualizar(){
+     this.usuario.tipo = this.tipoUser
+
+     if(this.usuario.senha != this.confirmarSenha){
+       alert("Senhas incorretas")
+     } else{
+       this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+         this.usuario = resp
+         this.router.navigate(["/inicio"])
+         alert("Atualização feita com sucesso! Faça login novamente para ver as atualizações.")
+         environment.token = ""
+         environment.foto = ""
+         environment.nome = ""
+         environment.id = 0
+         this.router.navigate(["/entrar"])
+       })
+     }
+
+   }
+
+   findByIdUser(id: number){
+     this.authService.getByIdUser(id).subscribe((resp: Usuario) => {
+       this.usuario = resp
+     })
+   }
+
+}
