ae9ee3a01293bb08c5a99e575f01014bcd209469
Leonardo <you@example.com>
2021-02-01 09:33:46 -0300

    Adding search methods

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -19,10 +19,12 @@ export class InicioComponent implements OnInit {
 
   postagem: Postagem = new Postagem()
   listaPostagens: Postagem[]
+  tituloPost: string
 
   tema: Tema = new Tema()
   idTema: number
   listaTemas: Tema[]
+  nomeTema: string
 
   usuario: Usuario = new Usuario
   idUser = environment.id
@@ -75,6 +77,27 @@ export class InicioComponent implements OnInit {
       this.usuario = resp
     })
 
+  }
+
+  findByTituloPostagem(){
+
+    if(this.tituloPost == ""){
+      this.getAllPostagens()
+    }else{
+      this.postagemService.getByTituloPostagem(this.tituloPost).subscribe((resp: Postagem[]) => {
+      this.listaPostagens = resp
+    })
+    }
+  }
+
+  findByNomeTema(){
+    if(this.nomeTema == ""){
+      this.getAllTemas()
+    } else{
+      this.temaService.getByNomeTema(this.nomeTema).subscribe((resp: Tema[]) => {
+        this.listaTemas = resp
+      })
+    }
   }
 
   publicar(){
